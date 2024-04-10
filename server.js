6b649912a88e8ea9326930928251f1c89f98e495
Nofar <nofarsh7@gmail.com>
2024-04-11 01:48:07 +0300

    MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const port = 3000;
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(express.json()); 
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  // You can optionally handle requests to the root URL separately here
+  res.send('Hello from the root URL!');
+});
+
+
+
+
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
